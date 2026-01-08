d11dac2a8aa5ba754d1c97ab790f64cec7c41b3b
coryeni <iancor2514@gmail.com>
2026-01-08 02:31:48 -0600

    Quitar Local

--- database.py
+++ database.py
@@ -4,24 +4,19 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
-if DATABASE_URL:
-    # Render usa postgres:// y SQLAlchemy necesita postgresql://
-    if DATABASE_URL.startswith("postgres://"):
-        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL no está definida. Render no inyectó la variable.")
 
-    engine = create_engine(
-        DATABASE_URL,
-        echo=False,
-        pool_pre_ping=True,
-        connect_args={"sslmode": "require"}  # 🔴 OBLIGATORIO EN RENDER
-    )
-else:
-    # LOCAL
-    engine = create_engine(
-        "postgresql+psycopg2://postgres:postgres@localhost:5435/club_lectura",
-        echo=False,
-        pool_pre_ping=True
-    )
+# Render usa postgres://, SQLAlchemy requiere postgresql://
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+engine = create_engine(
+    DATABASE_URL,
+    echo=False,
+    pool_pre_ping=True,
+    connect_args={"sslmode": "require"}
+)
 
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
